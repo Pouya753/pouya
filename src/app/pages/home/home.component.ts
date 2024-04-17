5be5a8c1eef12270e4dcddb8616d2ee80f9d2bc9
Pouya753 <shiripoya@gmail.com>
2024-04-17 02:00:42 -0700

    attribute binding

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  isCorrect: boolean = true;
+isCancelled:boolean=true
+  classes = {};
+
+  ngOnInit() {
+    this.classes = {
+      'p1': this.isCorrect,
+'p2':this.isCancelled,
+      
+    }
+  }
+
 
 }
